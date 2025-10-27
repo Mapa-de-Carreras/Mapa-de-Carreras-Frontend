bd04340466289877748eee5086009b5c36ed19e7
felix-op <felixoscarperezsegovia@gmail.com>
2025-10-27 07:15:43 -0300

    feat: se agrega un ThemeProvider para cambiar el tema

--- src/index/index.tsx
+++ src/index/index.tsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import Router from './Router'
 import './index.css'
+import { ThemeProvider } from '@components/Providers/ThemeProvider';
 
 const root = document.getElementById('root');
 
 createRoot(root!).render(
-    <Router />
+    <ThemeProvider>
+        <Router />
+    </ThemeProvider>
 )
