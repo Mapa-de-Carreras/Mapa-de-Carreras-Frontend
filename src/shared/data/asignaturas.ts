8b5901be33bddcf985d999b49d2570d85332c176
Maxitoto <tommyush2000@hotmail.com>
2025-11-05 10:30:41 -0300

    se agrego datos de asignaturas

---
+++ src/shared/data/asignaturas.ts
@@ -0,0 +1,72 @@
+import Asignatura  from "@globalTypes/asignatura";
+
+
+const Asignaturas: Asignatura[] = [
+
+    {
+        "titulo": "Programación funcional",
+        "instituto": {
+            "codigo": 0,
+            "titulo": "IDEI"
+        },
+        "carrera": {
+            "codigo": 0,
+            "instituto": {
+                "codigo": 0,
+                "titulo": "IDEI"
+            },
+            "titulo": "Licenciatura en Sistemas",
+            "coordinador": "Ezequiel Moyano"
+        },
+        "codigo": "POO23",
+        "anio": "4ro",
+        "tipo": "Cuatrimestral",
+        "etapa": "1er",
+        "comision": "Vespertino"
+    },
+
+    {
+        "titulo": "Programación Orientada a Objetos",
+        "instituto": {
+            "codigo": 0,
+            "titulo": "IDEI"
+        },
+        "carrera": {
+            "codigo": 0,
+            "instituto": {
+                "codigo": 0,
+                "titulo": "IDEI"
+            },
+            "titulo": "Licenciatura en Sistemas",
+            "coordinador": "Ezequiel Moyano"
+        },
+        "codigo": "POO23",
+        "anio": "3ro",
+        "tipo": "Cuatrimestral",
+        "etapa": "2do",
+        "comision": "Vespertino"
+    },
+    {
+        "titulo": "Matemática Discreta",
+        "instituto": {
+            "codigo": 0,
+            "titulo": "IDEI"
+        },
+        "carrera": {
+            "codigo": 0,
+            "instituto": {
+                "codigo": 0,
+                "titulo": "IDEI"
+            },
+            "titulo": "Licenciatura en Sistemas",
+            "coordinador": "Ezequiel Moyano"
+        },
+        "codigo": "MD23",
+        "anio": "1ro",
+        "tipo": "Cuatrimestral",
+        "etapa": "1er",
+        "comision": "Matutino"
+    }
+]
+
+export default Asignaturas;
