432cc1011233a51610daa068107aae4653bc561c
Maxitoto <tommyush2000@hotmail.com>
2025-11-05 22:46:56 -0300

    update pagina de asignaturas

--- src/modules/academic/SubjectPage/SubjecPage.tsx
+++ src/modules/academic/SubjectPage/SubjecPage.tsx
@@ -1,103 +1,61 @@
-import PageBase from "@components/PageBase/PageBase";
-import asignaturas from "@data/asignaturas";
-import { Tabla } from "@components/Tabla/Tabla";
-import { ColumnDef } from "@tanstack/react-table";
-import TituloTabla from "@components/Tabla/TituloTabla";
-import AccionTabla from "@components/Tabla/AccionTabla";
-import Asignatura from "@globalTypes/asignatura";
+import PageBase from '@components/PageBase/PageBase'
+import asignaturas from '@data/asignaturas'
+import { Tabla } from '@components/Tabla/Tabla'
+import { ColumnDef } from '@tanstack/react-table'
+import TituloTabla from '@components/Tabla/TituloTabla'
+import AccionTabla from '@components/Tabla/AccionTabla'
+import Asignatura from '@globalTypes/asignatura'
 
 export default function SubjectPage() {
+	const handleVerDetalle = (codigo: string) => {
+		console.log('Asignatura a ver detalle codigo: ', codigo)
+	}
 
-    const handleVerDetalle = (codigo: string) => {
-        console.log("Asignatura a ver detalle codigo: ", codigo);
-    }
+	const columns: ColumnDef<Asignatura>[] = [
+		{
+			accessorKey: 'nombre',
+			header: ({ column }) => <TituloTabla column={column} titulo="Nombre" />,
+			cell: ({ row }) => (
+				<div className="flex flex-wrap font-medium">{row.getValue('nombre')}</div>
+			),
+		},
+		{
+			accessorKey: 'codigo',
+			header: ({ column }) => <TituloTabla column={column} titulo="Código" />,
+		},
+		{
+			accessorKey: 'cuatrimestre',
+			header: ({ column }) => <TituloTabla column={column} titulo="Cuatrimestre" />,
+		},
+		{
+			accessorKey: 'tipo_duracion',
+			header: ({ column }) => <TituloTabla column={column} titulo="Duración" />,
+		},
+		{
+			accessorKey: 'tipo_asignatura',
+			header: ({ column }) => <TituloTabla column={column} titulo="Tipo" />,
+		},
+		{
+			accessorKey: 'horas_semanales',
+			header: ({ column }) => <TituloTabla column={column} titulo="Hs. Semanales" />,
+		},
+		{
+			id: 'actions',
+			header: 'Acciones',
+			cell: ({ row }) => (
+				<AccionTabla onClick={() => handleVerDetalle(row.original.codigo)} />
+			),
+		},
+	]
 
-    const columns: ColumnDef<Asignatura>[] = [
-        {
-
-            accessorKey: "asignatura",
-            header: ({ column }) => (
-                <TituloTabla column={column} titulo="Asignatura" />
-            ),
-            cell: ({ row }) => (
-                <div className="flex flex-wrap font-medium">
-                    {row.getValue("asignatura")}
-                </div>
-            ),
-
-            size: 1,
-        },
-        {
-
-            accessorFn: (row) => row.instituto.titulo,
-            id: "instituto",
-            header: ({ column }) => (
-                <TituloTabla column={column} titulo="Instituto" />
-            ),
-            cell: ({ row }) => (
-                <div className="w-full flex flex-wrap font-medium">
-                    {row.getValue("instituto")}
-                </div>
-            ),
-        },
-        {
-    
-            accessorFn: (row) => row.carrera.titulo,
-            id: "carrera",
-            header: ({ column }) => (
-                <TituloTabla column={column} titulo="Carrera" />
-            ),
-            cell: ({ row }) => (
-                <div className="w-full flex flex-wrap font-medium">
-                    {row.getValue("carrera")}
-                </div>
-            ),
-        },
-        {
-            accessorKey: "anio",
-            header: ({ column }) => (
-                <TituloTabla column={column} titulo="Año" />
-            ),
-        },
-        {
-            accessorKey: "tipo",
-            header: ({ column }) => (
-                <TituloTabla column={column} titulo="Tipo de Ciclo" />
-            ),
-        },
-        {
-            accessorKey: "etapa",
-            header: ({ column }) => (
-                <TituloTabla column={column} titulo="Ciclo" />
-            ),
-        },
-        {
-            accessorKey: "comision",
-            header: ({ column }) => (
-                <TituloTabla column={column} titulo="Comision" />
-            ),
-        },
-        {
-            id: "actions",
-            header: "Acciones",
-            cell: ({ row }) => (
-                <AccionTabla onClick={() => handleVerDetalle(row.original.codigo)} />
-            ),
-        }
-    ]
-
-    return (
-        <PageBase
-            titulo="Página de Asignaturas"
-            subtitulo="Listado de Asignaturas"
-        >
-            <Tabla
-                columnas={columns}
-                data={asignaturas}
-                habilitarBuscador={true}
-                habilitarPaginado={true}
-                columnasFijas
-            />
-        </PageBase>
-    )
+	return (
+		<PageBase titulo="Página de Asignaturas" subtitulo="Listado de Asignaturas">
+			<Tabla
+				columnas={columns}
+				data={asignaturas}
+				habilitarBuscador={true}
+				habilitarPaginado={true}
+			/>
+		</PageBase>
+	)
 }
