e40fa21d15b605eff3b395bd5d70026ab1e677a8
felix-op <felixoscarperezsegovia@gmail.com>
2025-11-19 01:12:54 -0300

    feat: se agregan endpoint para roles y usuarios

---
+++ src/shared/apis/roles.ts
@@ -0,0 +1,11 @@
+import { Rol } from "@globalTypes/rol";
+import { URL_API } from "./constantes";
+import { useGet } from "./hooks/useGet";
+
+export function useGetRoles() {
+    return useGet<Rol[]>({
+        key: 'useGetRoles',
+        urlApi: `${URL_API}roles`,
+        isEnabled: true,
+    });
+}
