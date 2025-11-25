b0884fbc0d376092d5aed9ed45a1b117d5cabf70
Elias <elias63856@gmail.com>
2025-11-24 21:32:31 -0300

    feat:se agrega detalle comisiones ,editar ,agregar y se arregla error en campo select

--- src/index/Router.tsx
+++ src/index/Router.tsx
@@ -48,6 +48,9 @@ import AgregarDocumentos from '../modules/Documentos/AgregarDocumentos'
 import useRol from '@hooks/useRol'
 import DetalleDocumentos from '../modules/Documentos/DetalleDocumentos'
 import PaginaComisiones from '@academica/Comisiones/PaginaComisiones'
+import ComisionesDetalle from '@academica/Comisiones/ComisionesDetalle'
+import ComisionEditar from '@academica/Comisiones/ComisionesEditar'
+import ComisionesAgregar from '@academica/Comisiones/ComisionesAgregar'
 
 
 export default function Router() {
@@ -110,6 +113,9 @@ export default function Router() {
 								{ path: 'planes/editar/:id', Component:PlanEstudioEditar, menu: false },
 								{ path: 'planes/agregar', Component: PlanEstudioAgregar, menu: false },
 								{ path: 'comisiones', label: 'Comisiones', icon: 'icon-[mdi--bookshelf]', Component: PaginaComisiones, menu: true, },
+								{ path: 'comisiones/agregar/', Component: ComisionesAgregar, menu: false },
+								{ path: 'comisiones/detalle/:id', Component: ComisionesDetalle, menu: false },
+								{ path: 'comisiones/editar/:id', Component:ComisionEditar, menu: false },
 							],
 							menu: true,
 						},
---
+++ src/shared/types/planasignatura.ts
@@ -0,0 +1,13 @@
+export interface IPlanAsignatura {
+  id: number;                   
+  plan_de_estudio_id: number;    
+  asignatura_id: number;         
+  anio: number | null;            
+  horas_teoria: number | null;    
+  horas_practica: number | null; 
+  horas_semanales: number | null; 
+  horas_totales: number;         
+  created_at: string;            
+  updated_at: string;            
+  descripcion: string;            
+}
