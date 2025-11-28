9664f6bac7f7138e038e24f64bb78e6821da142b
Maxitoto <tommyush2000@hotmail.com>
2025-11-28 20:16:59 -0300

    modifique ruta para ver el editar docente

--- src/modules/Docentes/DocenteDetalle.tsx
+++ src/modules/Docentes/DocenteDetalle.tsx
@@ -45,7 +45,7 @@ export default function DocenteDetalle() {
 
   const handleEditar = () => {
     if (!docente) return;
-    navigate(`/docentes/editar/${id}/`)
+    navigate(`/docentes/gestion/editar/${id}`);
   };
 
     const handleCerrarModal = () => {
